9f9f9a0e65ae08d135c0a981c73277a6368ba5b6
rahul masih <rahulmasih61@gmail.com>
2019-10-04 12:21:30 +0530

    completed

--- bin/main.dart
+++ bin/main.dart
@@ -15,7 +15,21 @@
 //    [5, 5, 5, 5, 5],
 //    [5, 5, 5, 5, 5]
 //  ]
-//
+void matrix(int n) {
+  List<List<int>> outer = [];
+  List<int> inner = [];
+  for (int i = 1; i <= n; i++) {
+    inner= [];
+    for (int j = 1; j <= n; j++) {
+      inner.add(n);
+    }
+    outer.add(inner);
+  }
+
+  for (List x in outer) {
+    print(x);
+  }
+}
 //squarePatch(1) should print
 //  [
 //    [1]
@@ -24,7 +38,6 @@
 // squarePatch(0) should print []
 
 main() {
-
   List<List<String>> board = [
     ['O', ' ', ' '],
     [' ', 'X', ' '],
@@ -41,6 +54,7 @@ main() {
   // 3  X |   |
 
   board[0][2] = 'O';
+  print('----------------------------------------');
   printBoard(board);
   // The above call should print:
   //    A   B   C
@@ -49,7 +63,20 @@ main() {
   // 2    | X |
   //   ---+---+---
   // 3  X |   |
+  matrix(4);
+
 }
+
+void printBoard(List<List<String>> list) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j <=0; j++){
+      print(list[i].join(' | '));
+      continue;
+    }
+    print('--+---+--');
+  }
+}
+
 
 // Write a function called 'printBoard' which takes a List of List of Strings,
 // and prints a Tic Tac Toe board to the console.  The board will always
